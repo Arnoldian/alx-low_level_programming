fe6a833c2581dc936da9a9cdcc1b3df26fa8d2c4
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-03-16 20:48:42 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* uses puts function to print string */
+/* returns zero at the end of main function */
+int main()
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
