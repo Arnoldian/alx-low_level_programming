387a5c3ab7370d714151280aa95f31c2fbc11588
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-03-16 20:53:59 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* printf funct to print string */
+/* return 0 at end of main funct */
+int main ()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
