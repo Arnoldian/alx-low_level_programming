977f101c6de824d5e75ee713e3cc5a851018bc7a
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-03-23 20:44:37 -0700

    task 2 and main.h update 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: multiplied with  b
+ * @b: mutiplied with a
+ * Return: the result of the operation
+ */
+
+int mul(int a, int b)
+{
+        int c;
+
+        c = a * b;
+
+        return (c);
+}
