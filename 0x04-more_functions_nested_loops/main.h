8c521e31415b3904d42af4b9890e302eb16fcce8
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-03-24 19:20:35 -0700

    task 10 and main.h update

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,11 +5,12 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, in b);
 void print_numbers(void);
-int _putchar(char c);		/** necessary from task 3 */
+int _putchar(char c);		/** necessary from task 3 excluding 9 */
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
 
 #endif
