0e1ae5287b4f1c3aad480db22e6f956fe61c5278
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-03-24 18:42:42 -0700

    main.h _putchar prototype

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, in b);
 void print_numbers(void);
+int _putchar(char c);		/** necessary from task 3 */
 void print_most_numbers(void);
 
 #endif
