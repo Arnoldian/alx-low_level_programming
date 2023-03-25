c3dca543a7ad5e2cb678e881f523c71bf0e1a57b
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-03-24 19:34:10 -0700

    task 12

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,12 +5,13 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, in b);
 void print_numbers(void);
-int _putchar(char c);		/** necessary from task 3 excluding 9 */
+int _putchar(char c);		/** necessary from task 3 excluding 9 and 11 */
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 
 #endif
