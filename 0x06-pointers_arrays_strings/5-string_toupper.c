661dd1ce7a9f9fdabb7556bf6b3c7e9774a58790
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-03-30 16:55:27 -0700

    task 5, main.h update

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - function, lowercase letters of string to uppercase
+ * @s: input string parameter pointer
+ * Return: s
+ */
+
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+
+		count++;
+	}
+
+	return (s);
+}
