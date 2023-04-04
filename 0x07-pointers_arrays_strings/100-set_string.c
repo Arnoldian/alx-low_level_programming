1c46ec960029f390979912c7e325f7347c6048ef
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-04-03 19:36:21 -0700

    task 8, main.h update

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - func sets value of pointer to char
+ * @s: source adress
+ * @to: target adress
+ * Return: no return statement
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
