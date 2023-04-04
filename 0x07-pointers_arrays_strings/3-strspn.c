1b169af6713b280fe9d8f5f176713403a367d6f5
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-04-03 18:58:47 -0700

    task 3, main.h update

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - function gets the length of prefix substring
+ * @s: string 
+ * @accept: bytes
+ * Return: i
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, bool;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		bool = 1;
+
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+
+		if (bool == 1)
+			break;
+	}
+
+	return (i);
+}
