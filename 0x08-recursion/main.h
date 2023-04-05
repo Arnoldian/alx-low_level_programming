f8090fc1f5a37a802407db296fa5289977df425d
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-04-05 12:00:14 -0700

    main.h update

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,4 +6,4 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int exponentiation(int n, int c);	/* my addition  (not exercise intrinsic) */ 
+int exponentiation(int n, int c);
