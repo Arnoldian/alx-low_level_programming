bcb095c71f1a156291e26c6cd2144ca0651cc8fc
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-04-17 13:46:49 -0700

    task 0

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,8 +2,8 @@
  * ABS(x) - func-like macro computes abs val
  * of number x
  */
-#ifndef _FUNCTION_LIKE_MACRO_
-#define _FUNCTION_LIKE_MACRO_
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
 #define ABS(x) ((x) < (0)) ? -(x) : (x))
 
