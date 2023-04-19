13bfaab4914722e3e9bb9e919e86aebca3453f33
Nkosikhona Dlamini <nkosikhonadlamini@outlook.com>
2023-04-19 16:37:24 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - func prints name
+ * @name: name to print parameter
+ * @f: print function pointer parameter
+ * return: no return statement
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
