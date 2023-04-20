8bea274bfd214e692bb46689f60d464502700c77
Arnoldian <77880895+Arnoldian@users.noreply.github.com>
2023-04-20 18:38:33 -0400

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-/**
- * ABS(x) - func-like macro computes abs val
- * of number x
- */
-
-#define ABS(x) ((x) < (0)) ? -(x) : (x))
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,6 +0,0 @@
-/**
- * SUM(x, y) - func-like macro computs sum
- * of x and y
- */
-
-#define SUM(x, y) ((x) + (y))
