351c11f7d167367549094ade9b5107d12827b5b8
Nkosikkhona Dlamini <nkosikhonadlamini@outlook.com>
2023-06-19 12:13:28 -0700

    task 0 and README.md

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+/**
+ * struct dog - new type struct dog
+ * @name: name of it 
+ * @age: age of it
+ * @owner: owner of it
+ * description: dog with name, age and owner info
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog task 3
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
