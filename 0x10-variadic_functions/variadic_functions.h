6e67591ddbe39f5d2a27a7345e4cc1cfbe17e8a1
Nkosikkhona Dlamini <nkosikhonadlamini@outlook.com>
2023-06-22 16:15:21 -0700

    task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,4 +8,14 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * for task 3 specifically
+ */
+
+void print_char(va_list arg);
+void print_string(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+// void print_all(const char * const format, ...);
+
 #endif
