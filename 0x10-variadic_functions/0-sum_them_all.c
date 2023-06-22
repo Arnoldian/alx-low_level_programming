17bc3bb3bfa52b1584f9f9d4977ebad3bad97779
Nkosikkhona Dlamini <nkosikhonadlamini@outlook.com>
2023-06-22 15:29:53 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,11 +9,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
+	unsigned int i;
 	va_list num_list;
 
 	va_start(num_list, n);
 
-	for (unsigned int i = 0; i < n ; i++)
+	for (i = 0; i < n ; i++)
 		sum += va_arg(num_list, int);
 	va_end(num_list);
 	return (sum);
