b75f337699f5c155a5ea4ac3e7216cf0868745a0
Nkosikkhona Dlamini <nkosikhonadlamini@outlook.com>
2023-07-04 00:15:10 -0700

    touch ups huh

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stdlib.h>
+
+
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -16,5 +20,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);		/* Advanced tasts */
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
