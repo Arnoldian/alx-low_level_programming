a4e09dc165da6cf17a38bb5e42ec8b505cdbbbc5
Nkosikkhona Dlamini <nkosikhonadlamini@outlook.com>
2023-07-05 01:01:45 -0700

    task 2 and lists.h update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>	/* for task 2 and 3 */
 
 /**
  * struct list_s - singly linked list
@@ -21,4 +22,4 @@ typedef struct list_s
 int _putchar(char);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-
+list_t *add_node(list_t **head, const char *str);
