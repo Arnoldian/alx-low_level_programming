544d77d2f98f45db52b507b673d115e608f9d914
Nkosikkhona Dlamini <nkosikhonadlamini@outlook.com>
2023-07-05 00:42:44 -0700

    task 1 and lists.h update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of string
+ * @next: points to the next node
+ *
+ * Description: node structure of singly linked list
+ */
+
+/* struct made for tasks starting with task 1 */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 int _putchar(char);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
