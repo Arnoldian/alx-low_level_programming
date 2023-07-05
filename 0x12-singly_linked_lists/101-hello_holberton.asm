fa05613c59427df5c873ec955daa9a290f0bb6f3
Nkosikkhona Dlamini <nkosikhonadlamini@outlook.com>
2023-07-05 01:51:28 -0700

    task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+
+section .data
+    message db 'Hello, Holberton', 10
+
+section .text
+    global _start
+
+_start:
+    ; write message to stdout
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, message
+    mov edx, 16
+    int 0x80
+
+    ; exit program
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
