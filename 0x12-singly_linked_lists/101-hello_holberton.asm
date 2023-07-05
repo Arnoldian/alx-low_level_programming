f3b6485a5073b2131847b03b38f2915b5f57ef45
Nkosikkhona Dlamini <nkosikhonadlamini@outlook.com>
2023-07-05 02:13:09 -0700

    2nd round of touch ups

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,13 +6,13 @@ section .text
 	global main
 	extern printf
 
-_start:
-    ; write message to stdout
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, message
-    mov edx, 16
+main:
+	; write message to stdout
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, message
+	mov edx, 16
 
-    ; exit program
-    mov eax, 1
-    xor ebx, ebx
+	; exit program
+	mov eax, 1
+	xor ebx, ebx
