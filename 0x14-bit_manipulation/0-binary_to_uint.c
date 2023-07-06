0f7d5c58b66bcc8c1caaf7145652109da121569f
Nkosikkhona Dlamini <nkosikhonadlamini@outlook.com>
2023-07-06 13:44:15 -0700

    last round of touch ups

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,23 +5,32 @@
  * @b: points to binary
  * Return: converted num or 0
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
+    unsigned int outcome = 0;
 
-	if (b == NULL)
-		return (0);
+    if (b == NULL)
+    {
+        return 0;
+    }
 
-	while (*b)
+    for (int i = 0; b[i] != '\0'; i++)
+    {
+        if (b[i] == '0')
 	{
-		if (*b == '1')
-			decimal = (decimal << 1) | 1;
-		else if (*b == '0')
-			decimal <<= 1;
-		else
-			return (0);
-		b++;
-	}
+            outcome = outcome << 1;
+        }
+	else if (b[i] == '1')
+	{
+            outcome = (outcome << 1) | 1;
+        }
+	else
+	{
+            return 0;
+        }
+    }
 
-	return (decimal);
+    return outcome;
 }
+
