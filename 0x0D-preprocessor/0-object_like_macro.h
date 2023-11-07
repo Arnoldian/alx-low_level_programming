7a49e1bb8c6c21d7c2d68e0f5c45ccc67fcb051a
Nkosikhona <nkosikhonadlamini@outlook.com>
2023-11-06 16:20:07 -0800

    tasks 0 -> 4

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,5 @@
+/**
+ * SIZE - macro for token 1024 
+ * */
+
+#define SIZE 1024
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program prints name of file its compiled from
+ * followed by newline
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
