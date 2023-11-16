d4a1e2242fa22042f588338fc4e2b0ea07f24ce8
Nkosikhona <nkosikhonadlamini@outlook.com>
2023-11-16 04:10:01 -0800

    task 6 fiddling

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,48 +1,18 @@
-/* File: 101-hello_holberton.asm
-   This program prints "Hello, Holberton" with new line */
 
-/* .text section */
-.section .rodata
-.LC0:
-	.string "Hello, Holberton"  /* Define the string to be printed */
+section .data
+	message db 'Hello, Holberton', 10
 
-/* main function */
-.globl main
-.type main, @function
+section .text
+	global main
+	extern printf
+
 main:
-.LFB0:
-	.cfi_startproc
-	endbr64
-	pushq %rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq %rsp, %rbp
-	.cfi_def_cfa_register 6
-	leaq .LC0(%rip), %rdi  /* Load the address of the string into rdi */
-	call puts@PLT  /* Call the puts function to print the string */
-	movl $0, %eax  /* Set the return value to 0 */
-	popq %rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size main, .-main
-	.ident "GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-	.section .note.GNU-stack,"",@progbits
-	.section .note.gnu.property,"a"
-	.align 8
-	.long 1f - 0f
-	.long 4f - 1f
-	.long 5
-0:
-	.string "GNU"
-1:
-	.align 8
-	.long 0xc0000002
-	.long 3f - 2f
-2:
-	.long 0x3
-3:
-	.align 8
-4
+	; write message to stdout
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, message
+	mov edx, 16
 
+	; exit program
+	mov eax, 1
+	xor ebx, ebx
