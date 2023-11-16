ec105a0475e99633c9725b0ee7b5fae43def629c
Nkosikhona <nkosikhonadlamini@outlook.com>
2023-11-16 04:07:40 -0800

    task 6 fiddling

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,44 +1,48 @@
-	.file	"hello_holberton.c"
-	.text
-	.section	.rodata
+/* File: 101-hello_holberton.asm
+   This program prints "Hello, Holberton" with new line */
+
+/* .text section */
+.section .rodata
 .LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
+	.string "Hello, Holberton"  /* Define the string to be printed */
+
+/* main function */
+.globl main
+.type main, @function
 main:
 .LFB0:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
+	pushq %rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	movq %rsp, %rbp
 	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rdi
-	call	puts@PLT
-	movl	$0, %eax
-	popq	%rbp
+	leaq .LC0(%rip), %rdi  /* Load the address of the string into rdi */
+	call puts@PLT  /* Call the puts function to print the string */
+	movl $0, %eax  /* Set the return value to 0 */
+	popq %rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
 .LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
+	.size main, .-main
+	.ident "GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section .note.GNU-stack,"",@progbits
+	.section .note.gnu.property,"a"
 	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
+	.long 1f - 0f
+	.long 4f - 1f
+	.long 5
 0:
-	.string	 "GNU"
+	.string "GNU"
 1:
 	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
+	.long 0xc0000002
+	.long 3f - 2f
 2:
-	.long	 0x3
+	.long 0x3
 3:
 	.align 8
-4:
+4
+
