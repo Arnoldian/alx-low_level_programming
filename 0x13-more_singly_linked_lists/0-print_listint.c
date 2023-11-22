b705c7676d85c683397990c89a0faef46bb40e62
Nkosikhona <nkosikhonadlamini@outlook.com>
2023-11-21 16:25:44 -0800

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
 #include "lists.h"
+#include<stdio.h>
 
 /**
  * print_listint - prints all the elements of list
@@ -17,5 +17,6 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		num_nodes++;
 	}
+
 	return (num_nodes);
 }
