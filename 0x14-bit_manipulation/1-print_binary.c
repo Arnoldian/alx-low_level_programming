d110592a9f6fd22e3aff36bd2b54381a9e04f666
Nkosikhona <nkosikhonadlamini@outlook.com>
2023-11-23 10:04:36 -0800

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,28 +9,28 @@
 
 void print_binary(unsigned long int n)
 {
-    unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-    int flag = 0;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int flag = 0;
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-    while (mask > 0)
-    {
-        if ((n & mask) == 0)
-        {
-            if (flag == 1)
-                _putchar('0');
-        }
-        else
-        {
-            _putchar('1');
-            flag = 1;
-        }
-        mask = mask >> 1;
-    }
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+		{
+			if (flag == 1)
+			_putchar('0');
+		}
+		else
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		mask = mask >> 1;
+	}
 }
 
