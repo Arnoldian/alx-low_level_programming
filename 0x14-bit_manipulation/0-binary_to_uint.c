be0a0adb7872e94e16e20a93e2eb44bad0703960
Nkosikhona <nkosikhonadlamini@outlook.com>
2023-11-23 09:35:35 -0800

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,12 +11,12 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int index = 0, result = 0;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 
 	while (b[index] != '\0')
 	{
-		if ((b[index] != '0') || (b[index] != '1'))
-			return 0;
+		if ((b[index] != '0') && (b[index] != '1'))
+			return (0);
 		
 		result <<= 1;
 
@@ -26,5 +26,5 @@ unsigned int binary_to_uint(const char *b)
 		index += 1;
 	}
 
-	return result;
+	return (result);
 }
