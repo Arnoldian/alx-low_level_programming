5abea9858574ef4521df29089eaa541d778db1df
Nkosikhona <nkosikhonadlamini@outlook.com>
2023-11-23 10:12:55 -0800

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,15 +4,16 @@
  * get_bit - returns val of bit at specified index
  * @n: num to extract bit from
  * @index: index of bit to get
- * Return: val of bit @ given index or -1
+ * Return: val of bit at given index, or -1 if error
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8) {
-        return -1; // Error: Index out of range
-    }
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return -1; // error: out of range index
+	}
 
-    unsigned long int mask = 1UL << index;
-    return (n & mask) ? 1 : 0;
+	unsigned long int mask = 1UL << index;
+	return (n & mask) ? 1 : 0;
 }
