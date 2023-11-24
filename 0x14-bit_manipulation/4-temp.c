98dd6f6e002b75153e2d03c4a5adfcb9b1ac66ac
Nkosikhona <nkosikhonadlamini@outlook.com>
2023-11-23 17:10:56 -0800

    task 4

--- 0x14-bit_manipulation/4-temp.c
+++ 0x14-bit_manipulation/4-temp.c
@@ -14,9 +14,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 		return -1; // Error: Index out of range
 	}
 
-	m = 1 << index;
-
-	if (*n & m)
-		*n ^= m;
+	*n = *n & ~(1UL << index);
 	return 1;
 }
