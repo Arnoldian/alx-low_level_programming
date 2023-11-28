375a37b414221522026f8fe608cce6d9e2d6091e
Nkosikhona <nkosikhonadlamini@outlook.com>
2023-11-27 18:16:37 -0800

    task touch ups

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -26,16 +26,16 @@ void print_elf_header(Elf64_Ehdr *header)
 	printf("  Magic:   ");
 	for (int i = 0; i < EI_NIDENT; i++)
 	{
-		printf("%02x ", header->e_ident[i]);
+		printf("%02x", header->e_ident[i]);
 	}
 	printf("\n");
-	printf("Class: %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("Data: %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("Version: %d (current)\n", header->e_ident[EI_VERSION]);
-	printf("OS/ABI: %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "UNIX - NetBSD");
-	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("Type: %s\n", header->e_type == ET_EXEC ? "EXEC (Executable file)" : "DYN (Shared object file)");
-	printf("Entry point address: 0x%lx\n", header->e_entry);
+	printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+	printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "UNIX - NetBSD");
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %s\n", header->e_type == ET_EXEC ? "EXEC (Executable file)" : "DYN (Shared object file)");
+	printf("  Entry point address:               0x%lx\n", header->e_entry);
 }
 
 /**
