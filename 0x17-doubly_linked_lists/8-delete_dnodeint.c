6ac4f1637367275bff1074c89beb89a63d996c4b
Nkosikhona <nkosikhonadlamini@outlook.com>
2024-01-11 14:40:07 -0800

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,11 +13,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int counter = 0;
 
 	if (head == NULL || *head == NULL)
+	{
 		return (-1);
+	}
 
 	if (index == 0)
 	{
 		*head = temp->next;
+
 		if (temp->next == NULL)
 		{
 			return (-1);
